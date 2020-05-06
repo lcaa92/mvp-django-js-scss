f7c44a8b5a5b8f40e5c6630d79a8fe2218a64021
Leandro Araújo <leandrocaaraujo@gmail.com>
2020-05-06 19:38:19 -0300

    Initial commit; Core and dashboard app

---
+++ dashboard/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.contrib.auth.decorators import login_required
+
+from . import views
+
+urlpatterns = [
+    path('', login_required(views.index), name='dashboard_index'),
+]
