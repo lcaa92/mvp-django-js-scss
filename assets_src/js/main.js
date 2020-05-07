b40a53f7ac88325274c60992d8df2c44d47eac11
Leandro Araújo <leandrocaaraujo@gmail.com>
2020-05-06 22:36:22 -0300

    Template adinlte login and vendors assets

--- assets_src/js/main.js
+++ assets_src/js/main.js
@@ -1,2 +1,10 @@
+var $ = require( "jquery" );
+var jQuery = require( "jquery" );
+
+// export for others scripts to use
+window.$ = $;
+window.jQuery = jQuery;
 alert('TESTE')
+
 console.log('starting main.js watch')
+
