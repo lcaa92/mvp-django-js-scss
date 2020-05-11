74ef32b484e86de18c800e5b8e52d8a4b9bf437c
Leandro Araújo <leandrocaaraujo@gmail.com>
2020-05-11 20:37:33 -0300

    Template users

--- dashboard/views/administration.py
+++ dashboard/views/administration.py
@@ -1,11 +1,11 @@
-from django.shortcuts import reverse
+from django.shortcuts import reverse, render
 from django.http import HttpResponseRedirect
 
 
 # Create your views here.
 def users(request):
     print('users')
-    return HttpResponseRedirect(reverse('dashboard_index'))
+    return render(request, 'dashboard/administration/users.html')
 
 
 def groups(request):
