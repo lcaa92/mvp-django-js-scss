b97351efdb7bf0f10a109be19fc67460dc33642c
Leandro Araújo <leandrocaaraujo@gmail.com>
2020-05-16 09:52:19 -0300

    Remove console.log

--- assets_src/js/users.js
+++ assets_src/js/users.js
@@ -2,7 +2,6 @@ import 'datatables.net-bs4/css/dataTables.bootstrap4.min.css'
 var $  = require( 'jquery' );
 require( 'datatables.net-bs4' );
 
-console.log(ctx)
 $('#tableUsers').DataTable( {
     "ajax": {
       "url": ctx.data_url,
