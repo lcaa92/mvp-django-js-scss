deab6315d22416704dcf42c2c6d55c0cf46dd9b8
Leandro Araújo <leandrocaaraujo@gmail.com>
2020-11-10 09:39:38 -0300

    Flake 8 rules

--- djsite/urls.py
+++ djsite/urls.py
@@ -20,8 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dashboard/', include('dashboard.urls')
-    ),
-    path('', include('core.urls')
-    ),
+    path('dashboard/', include('dashboard.urls')),
+    path('', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
